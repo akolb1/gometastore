043c4fd284c37325fc3dbb2ca16b9db01133c8e3
Alexander Kolbasov <akolb@cloudera.com>
2018-07-10 23:38:25 -0700

    microbenchmarks - added stats examples.

--- microbench/benchmark_test.go
+++ microbench/benchmark_test.go
@@ -26,3 +26,19 @@ func ExampleMeasure() {
 		}, 1, 1)
 	// Output: pretest test cleanup pretest test cleanup
 }
+
+func ExampleStats_Max() {
+	stats := microbench.MakeStats()
+	stats.Add(1.0)
+	stats.Add(2.0)
+	fmt.Println("max =", stats.Max())
+	// Output max = 2.0
+}
+
+func ExampleStats_Min() {
+	stats := microbench.MakeStats()
+	stats.Add(1.0)
+	stats.Add(2.0)
+	fmt.Println("min =", stats.Min())
+	// Output max = 1.0
+}
